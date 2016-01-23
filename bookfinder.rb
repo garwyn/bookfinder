44fcd547b2963bf8ca52cde69e8b81c823617d4c
Gabriel Russell <gdrussell81@gmail.com>
2016-01-23 14:31:58 -0500

    added data persistance

--- bookfinder.rb
+++ bookfinder.rb
@@ -1,6 +1,8 @@
+#!/usr/bin/ruby
+
 require 'net/http'
 require 'json'
-require 'pstore'
+require 'csv'
 
 def get_author_name(data)
 	return data["data"][0]["author_data"][0]["name"]
@@ -15,16 +17,18 @@ def get_publish_date(data)
 	return published
 end
 
-store = PStore.new("data.pstore")# I think this is working, but how do I tell?
-
 print "Input your ISBN number here > "
 ISBN = gets.chomp
 
 url = "http://isbndb.com/api/v2/json/TVVFMNE5/book/#{ISBN}"
 resp = Net::HTTP.get_response(URI.parse(url))
-#Also, how do I put int the == 200 safeguard? I think it goes here but it blows up.
+ if resp.code == "200"
 	data = JSON.parse(resp.body)
 	puts JSON.pretty_generate(data)
+else
+	puts resp.code
+	puts "Something went wrong."
+end
 
 puts ("-" * 10 ) + ("\n" * 10) + ("-" * 10)
 
@@ -35,3 +39,14 @@ publsh_date = get_publish_date(data)
 puts "MY AUTHOR NAME: #{author_name}"
 puts "THIS IS THE TITLE: #{title}"
 puts "This was published on #{publsh_date}"
+
+book = {
+	:author => "#{author_name}",
+	:title => "#{title}",
+	:publish_date => "#{publsh_date}"
+}
+
+CSV.open("./bookfinder.csv", "a") do |csv|
+	csv << [book[:author], book[:title], book[:publish_date]]
+end
+
